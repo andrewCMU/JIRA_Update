081ecb61adf71c96954b859054f0a97fbae1b53a
Andrew Schwartz <aschwartz@atypon.com>
2020-02-29 17:16:33 -0500

    refactored to seperate different update types into their own methods. Also added error handling

--- JIRA_Update.py
+++ JIRA_Update.py
@@ -5,32 +5,47 @@ import pyjq
 import csv
 import base64
 import getpass
+import re
 
-def JIRA_request(update_type, ticket, parameter):
-	url = "https://jira.atypon.com/rest/api/latest/issue/" + ticket
-	if update_type == "comment":
-		url += "/comment"
-		d = {"body":str(parameter)}
-		r = requests.post(url, headers = header, data = json.dumps(d))
-	elif update_type == "assign":
-		url += "/assignee"
-		d = {"name":str(parameter)}
-		r = requests.put(url, headers = header, data = json.dumps(d))
-	
-	request_text = dump.dump_all(r)
-	print(request_text)
+def comment(update_type, ticket, parameter):
+	url = base_url + ticket + "/comment"
+	d = {"body":str(parameter)}
+	r = requests.post(url, headers = header, data = json.dumps(d))
+	response(r)
+
+def assign(update_type, ticket, parameter):
+	url = base_url + ticket + "/assignee"
+	d = {"name":str(parameter)}
+	r = requests.put(url, headers = header, data = json.dumps(d))
+	response(r)
+
+def response(r):
+	#print(r.content)
+	if(pattern.match(str(r.status_code))):
+		print("Succesfull Update")
+	else:
+		print("Failed Update: Error " + str(r.status_code))
+	pass
 
 # Collect User Input
-password = getpass.getpass(prompt="Enter JIRA credentials (username:password) ")
+print("Welcome to the JIRA Update Ticket. Please ensure you are connected to your VPN before continuing")
+password = getpass.getpass(prompt="Enter your JIRA credentials (username:password): ")
 auth = "Basic " + base64.b64encode(password.encode("utf-8")).decode('ascii')
 
 #Static Data
 header = {"Content-Type":"application/json", "Authorization":auth}
+base_url = "https://jira.atypon.com/rest/api/latest/issue/"
+pattern = re.compile('^20\d')
 
 #initiate
 with open('tickets.tsv') as tsv_file:
 	fieldnames = ['type', 'ticket', 'parameter']
 	tsv_reader = csv.DictReader(tsv_file, dialect='excel-tab', fieldnames=fieldnames)
 	for row in tsv_reader:
-			print(row['ticket'])
-			JIRA_request(row["type"], row['ticket'], row["parameter"])
+			print("****\n" +row['ticket'])
+			if (row["type"] == "comment"):
+				comment(row["type"], row['ticket'], row["parameter"])
+			elif (row["type"] == "assign"):
+				assign(row["type"], row['ticket'], row["parameter"])
+			else:
+				print("Invalid Update Type")
